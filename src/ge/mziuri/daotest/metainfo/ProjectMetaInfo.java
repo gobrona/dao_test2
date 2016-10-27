b9a1b75c02bf4794e8bbb91570b40f7f1228b47e
Iosebi Gobronidze <gobronidze.ioseb@gmail.com>
2016-10-27 19:01:14 +0400

    initial commit...

---
+++ src/ge/mziuri/daotest/metainfo/ProjectMetaInfo.java
@@ -0,0 +1,16 @@
+
+package ge.mziuri.daotest.metainfo;
+
+public class ProjectMetaInfo {
+    
+    public static final String sqlResourcesURL = "resources/sql/";
+    
+    public static final String imagesResourcesURL = "resources/images/";
+    
+    public static final String cantFindCountry = "ქვეყანა ასეთი სახელით ვერ მოიძებნა :(";
+    
+    public static final String cantFindCapital = "ასეთი ქვეყანა ან მისი დედაქალაქი ვერ მოიძებნა :(";   
+    
+    public static final String cantFindCity = "ასეთი ქალაქი ვერ მოიძებნა :(";
+    
+}
